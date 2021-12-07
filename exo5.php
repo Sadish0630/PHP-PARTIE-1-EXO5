3a1bb7505ae48a0195cbf2bfc3e27b5baba87293
audemar <audemarsadish@gmail.com>
2021-12-07 15:22:09 +0100

    exo5 terminé

---
+++ exo5.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css"/>
+    <title>PARTIE 1 EXO 5</title>
+</head>
+<body>
+    <p>
+   <?php
+   $number = 'rien';
+   echo $number ;
+   ?>
+   </p>
+   <p>
+   <?php
+   $number = '123';
+   echo $number ;
+   ?>
+   </p>
+
+</body>
+</html>
